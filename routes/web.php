05bad4435ba43e8bc3a567697948597c4b0c7465
Aleksei <aleksandr671@yandex.ua>
2021-01-31 23:40:06 +0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/categories', function() {
+    $data = request(['title', 'description']);
+    \App\Models\Category::create($data);
+});
+Route::get('/categories', function(){
+    $categories = \App\Models\Category::all();
+    return view('category.index', compact('categories'));
+});
